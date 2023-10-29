fa196e1691eace69934260493bdf098f57ed9cf9
omidoyin--Git <omidoyinayodeji@yahoo.com>
2023-10-29 16:29:43 +0100

    Include frontend vercel route in allowedOrigins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,8 +1,14 @@
 const allowedOrigins = [
-    'https://www.yoursite.com',
-    'http://127.0.0.1:5500',
-    'http://localhost:3500',
-    'http://localhost:3000'
+  "https://www.yoursite.com",
+  "http://127.0.0.1:5500",
+  "http://localhost:3500",
+  "http://localhost:3000",
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
+  "https://fullstack-todolist-frontend-f0h8rmi40-omidoyin.vercel.app/",
+  "https://fullstack-todolist-frontend-f0h8rmi40-omidoyin.vercel.app",
+  "https://www.fullstack-todolist-frontend-f0h8rmi40-omidoyin.vercel.app/",
+  "https://www.fullstack-todolist-frontend-f0h8rmi40-omidoyin.vercel.app",
 ];
 
 module.exports = allowedOrigins;
--- middleware/Credentials.js
+++ middleware/Credentials.js
@@ -1,9 +1,13 @@
-
-const Credentials = (req, res, next ) => {
-  if(req?.headers) {res.header('Access-Control-Allow-Credentials',true);
-}
+const Credentials = (req, res, next) => {
+  try {
+    if (req?.headers) {
+      res.header("Access-Control-Allow-Credentials", true);
+    }
+  } catch (err) {
+    res.json("cors issues", err);
+  }
 
   next();
-}
+};
 
-module.exports = Credentials
+module.exports = Credentials;
